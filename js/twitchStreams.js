e37b4d62554f398c8a9ec493e35253603fbebf9a
Nils Devine <divinentd@gmail.com>
2015-11-10 19:35:40 -0800

    functional prototype, form submits, returns and displays results

--- js/twitchStreams.js
+++ js/twitchStreams.js
@@ -3,22 +3,93 @@ var SonyNils = SonyNils || {};
 SonyNils.TwitchStream = (function (Util) {
     "use strict";
 
-    var BASE_URL = "https://api.twitch.tv/kraken/search/streams",
-        CALLBACK_NAME = "SonyNils.TwitchStream.recieveData";
+    var BASE_URL = "https://api.twitch.tv/kraken/search/streams?limit=5",
+        CALLBACK_NAME = "SonyNils.TwitchStream.receiveData",
+        FORM_ID = "game-search-form",
+        SEARCH_FIELD_ID = "game-title",
+        RESULTS_ID = "twitchResults",
+        formElement,
+        searchInput;
 
-    function recieveData(json) {
-        console.log("recieveData", json);
+    function init() {
+        attachEvents();
+        Util.hideLoading();
+    }
+
+    function attachEvents() {
+        formElement = document.getElementById(FORM_ID);
+        searchInput = document.getElementById(SEARCH_FIELD_ID);
+
+        Util.addEvent(formElement, "submit", readForm);
+        //TODO: add pagination event
+    }
+
+    function readForm(e) {
+        e.preventDefault();
+        getData(searchInput.value);
     }
 
     function getData(query) {
+        Util.showLoading();
+
         var url = Util.addQueryToUrl(BASE_URL, query);
         Util.getJson(url, CALLBACK_NAME);
     }
+
+    function receiveData(results) {
+        console.log("receiveData", results);
+
+        var resultsTarget = document.getElementById(RESULTS_ID),
+            resultElements = document.createDocumentFragment();
+
+        if (results._total > 0) {
+            resultElements.appendChild(
+                buildPagination(results._total, results._links)
+            );
+            resultElements.appendChild(
+                buildResults(results.streams)
+            );
+        }
+        else {
+            //TODO: build no results element
+            alert("nothing!");
+        }
+
+        resultsTarget.innerHTML = "";
+        resultsTarget.appendChild(resultElements);
+
+        Util.hideLoading();
+    }
+
+    function buildPagination(count, links) {
+        var pagination = document.createElement("div");
+        pagination.innerHTML = "Total results: " + count;
+
+        return pagination;
+    }
+
+    function buildResults (streams) {
+        var list = document.createElement("ol"),
+            item,
+            i,
+            l;
+
+        for (i = 0, l = streams.length; i < l; i++) {
+            item = document.createElement("li");
+            item.innerHTML = streams[i].game;
+            list.appendChild(item);
+        }
+
+        return list;
+    }
     
+    /**
+     * exposing public methods
+     */
     return {
-        'getData' : getData,
-        'recieveData' : recieveData
+        'init' : init,
+        'receiveData' : receiveData
     };
 }(SonyNils.Utils));
 
-SonyNils.TwitchStream.getData('starcraft');
+SonyNils.TwitchStream.init();
--- js/utls.js
+++ js/utls.js
@@ -2,13 +2,15 @@
 var SonyNils = SonyNils || {};
     
 SonyNils.Utils = SonyNils.Utils || {
+    LOADING_ID : "loading",
+
     getJson : function (url, callbackName) {
         url = this.addCallbackToUrl(url, callbackName);
         this.insertJsonScript(url);
     },
 
     addQueryToUrl : function (base, query) {
-        return base + "?q=" + query;
+        return base + "&q=" + query;
     },
 
     addCallbackToUrl : function (url, callbackName) {
@@ -19,5 +21,26 @@ SonyNils.Utils = SonyNils.Utils || {
         var jsonPScript = document.createElement('script');
         jsonPScript.src = url;
         document.getElementsByTagName('head')[0].appendChild(jsonPScript);
+    },
+
+    hideLoading : function () {
+        var loadingDiv = this.loadingDiv || document.getElementById(this.LOADING_ID);
+        loadingDiv.style.display = "none";
+    },
+
+    showLoading : function () {
+        var loadingDiv = this.loadingDiv || document.getElementById(this.LOADING_ID);
+        loadingDiv.style.display = "";
+    },
+
+
+    addEvent : function (element, event, action) {
+        if (window.addEventListener) {
+            element.addEventListener(event, action, true);
+            return;
+        }
+        if (window.attachEvent) {
+            window.attachEvent("on" + event, action);
+        }
     }
 };
